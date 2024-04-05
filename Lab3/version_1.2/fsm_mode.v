229aeb24abb032fa9572f739be32a5b98b424bd4
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2024-04-05 16:23:50 +0700

    .

--- Lab3/version_1.2/fsm_mode.v
+++ Lab3/version_1.2/fsm_mode.v
@@ -2,27 +2,26 @@ module fsm_mode(
     input clk,
     input rst,
     input mode_button,
-    output reg [3:0] state
+    output reg [2:0] state
 );
     
-reg [3:0] state_next;
+reg [2:0] state_next;
 
-    parameter NORMAL = 000;
-    parameter SS = 001;
-    parameter MI = 010;
-    parameter HH = 011;
-    parameter DD = 100;
-    parameter MO = 101;
-    parameter YY = 110;
+    parameter NORMAL = 3'b000;
+    parameter SS = 3'b001;
+    parameter MI = 3'b010;
+    parameter HH = 3'b011;
+    parameter DD = 3'b100;
+    parameter MO = 3'b101;
+    parameter YY = 3'b110;
 
 always@(posedge clk or negedge rst)
 begin 
     if(~rst)
-      state <= 0;
+      state <= NORMAL;
     else 
        state <= state_next;
        
-    /////
      case(state)
        NORMAL: if(mode_button)
                    state_next <= SS;
